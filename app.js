4a3a4a7090958467f76251c683f54210a4d8ed83
LeoZion <leozionsuporte@gmail.com>
2022-08-08 18:32:22 -0300

    introduction to components

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+Vue.component('click-counter', {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  template: '<button @click="count++" class="border border-gray-300 rounded-md py-2 px-4">{{count}}</button>'
+})
 new Vue({
   el: '#app'
 })
