75dbde2b0de02a7f9b5ed4910d7c4ae4ec934726
LeoZion <leozionsuporte@gmail.com>
2022-08-09 06:15:04 -0300

    component slots

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
-let PlanPickerItemComponent =  {
-  template: '#plan-picker-item-template',
-  props: {
-    name: {
-      type: String,
-      required: true
-    },
-    selectedPlan: {
-      type: String
-    }
-  },
-  computed: {
-    isSelected() {
-      return this.name === this.selectedPlan
-    }
-  },
-  methods: {
-    select() {
-      this.$emit('select', this.name)
-    }
-  }
-}
-
-let PlanPickerComponent = {
-  template: '#plan-picker-template',
-  components: {
-    'plan-picker-item': PlanPickerItemComponent
-  },
+Vue.component('todo-item', {
+  template: '#todo-item',
   data() {
     return {
-      plans: ['The Single', 'The Curious', 'The Addict', 'The Alpes Gourmet'],
-      selectedPlan: null
-    }
-  },
-  methods: {
-    selectPlan(plan) {
-      this.selectedPlan = plan
+      completed: false
     }
   }
-}
+})
 
 new Vue({
-  el: '#app',
-  components: {
-    'plan-picker': PlanPickerComponent
-  }
+  el: '#app'
 })
